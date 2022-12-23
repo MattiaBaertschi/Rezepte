315e7a1bd385d08493c8ef11b5592f920b41b33c
TimonKuhn <timon.kuhn@students.fhnw.ch>
2022-12-23 16:12:33 +0100

    test änderung

--- react_project/src/Home.jsx
+++ react_project/src/Home.jsx
@@ -5,7 +5,7 @@ class Home extends Component {
     render () {
         return (
             <div className='w-full'>
-                <h1 className='my-10'>Timon und Mattias Rezepte</h1> 
+                <h1 className='my-10'>Rezeptesammlung von Mattia Bärtschi und Timon Kuhn</h1> 
                 <img src={ require("./bilder/Mattia_Baertschi.jpg")} alt=""  className='w-1/4 mx-auto font-family: Montserrat Alternates' />
             </div>
         )
